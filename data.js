ab972af79c46970594e2d70f745d1c3d9f7470cf
falgune2701 <falgunemondal2@gmail.com>
2025-09-29 11:02:55 +0530

    add 30+ viewpoint data into data.js and also write script for show viewpoint card dynamically

---
+++ data.js
@@ -0,0 +1,371 @@
+ export const viewPointsInfo = [
+    {
+        name: "Tiger Hill",
+        img_url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/98/14/61/witness-the-true-beauty.jpg?w=600&h=400&s=1",
+        loc_info:{
+            city:"Darjeeling",
+            state:"West Bengal"
+        },
+        moreInfoUrl: "https://en.wikipedia.org/wiki/Tiger_Hill,_Darjeeling"
+    },
+    {
+        name:"Nandi Hills",
+        img_url:"https://media.istockphoto.com/id/868559492/photo/nandi-hills-india.jpg?s=612x612&w=0&k=20&c=z2Owg07s7fyiCrK3F1DBgrgb_kerjWvUGp1DZIv3_rA=",
+        loc_info:{
+            city:"Chikkaballapur",
+            state:"Karnataka"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Nandi_Hills,_India"
+    },
+    {
+        name:"Rohtang Pass",
+        img_url:"https://jannattravelguru.com/wp-content/uploads/2023/03/Rohtang-pass.jpg",
+        loc_info:{
+            city:"Manali",
+            state:"Himachal Pradesh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Rohtang_Pass"
+    },
+    {
+        name:"Snow View Point",
+        img_url:"https://www.easeindiatrip.com/blog/wp-content/uploads/2024/12/Snow-View-Point-Nainital.jpg",
+        loc_info:{
+            city:"Nainital",
+            state:"Uttarakhand"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Nainital"
+    },
+    {
+        name:"Arthur's Seat",
+        img_url:"https://mediaim.expedia.com/destination/9/9524209b70cc9235f4b6013c1cb15f8f.jpg",
+        loc_info:{
+            city:"Edinburgh",
+            state:"Maharashtra"
+        },
+         moreInfoUrl:"https://en.wikipedia.org/wiki/Arthur%27s_Seat"
+    },
+    {
+        name:"Kufri View Point",
+        img_url:"https://moxtain.s3.ap-south-1.amazonaws.com/blogs/ThingsToDoInKufriValley/Kufri.jpg",
+        loc_info:{
+            city:"Shimla",
+            state:"Himachal Pradesh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Kufri,_India"
+    },
+    {
+        name:"Echo Point",
+        img_url:"https://static.toiimg.com/photo/msid-50465497,width-96,height-65.cms",
+        loc_info:{
+            city:"Munnar",
+            state:"Kerala"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Echo_Point"
+    },
+    {
+        name:"Anjaneri Hills",
+        img_url:"https://www.hlimg.com/images/things2do/738X538/Night-Trek-to-Anjaneri-Fort-Weekend-Thrill_1523273229t.jpg",
+        loc_info:{
+            city:"Nashik",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Anjaneri"
+    },
+    {
+        name:"Kodaikanal Pillar Rocks",
+        img_url:"https://www.karthitravels.com/images/new/kodaikanal/pillarrocks3.jpg",
+        loc_info:{
+            city:"Kodaikanal",
+            state:"Tamil Nadu"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Kodaikanal"
+    },
+    {
+        name:"Doddabetta Peak",
+        img_url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/3d/3d/ce/doddabetta-peak.jpg?w=1200&h=-1&s=1",
+        loc_info:{
+            city:"Ooty",
+            state:"Tamil Nadu"
+        },
+         moreInfoUrl:"https://en.wikipedia.org/wiki/Doddabetta"
+    },
+    {
+        name:"Coonoor Dolphin’s Nose",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiaNZCNOwU_Q5LXlgCaSY3MG3lMRSkgm95GQ&s",
+        loc_info:{
+            city:"Pelling",
+            state:"Sikkim"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Dolphin%27s_Nose,_Coonoor"
+    },
+    {
+        name:"Wilson Hills",
+        img_url:"https://www.tourismclub.in/eng/wp-content/uploads/2021/02/wilson-hills-hill-station.jpg",
+        loc_info:{
+            city:"Valsad",
+            state:"Gujarat"
+        },
+         moreInfoUrl:"https://en.wikipedia.org/wiki/Wilson_Hills"
+    },
+    {
+         name:"Tiffin Top",
+         img_url:"https://media.holidify.com/images/cmsuploads/compressed/image-24-1024x684_20241205173600.jpg",
+        loc_info:{
+            city:"Nainital",
+            state:"Uttarakhand"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Nainital"
+    },
+    {
+         name:"Gulmarg Gondola Peak",
+         img_url:"https://www.kashmironline.com/blog/wp-content/uploads/2022/06/gondola.jpg",
+        loc_info:{
+            city:"Gulmarg",
+            state:"Jammu & Kashmir"
+        },
+         moreInfoUrl:"https://en.wikipedia.org/wiki/Gulmarg_Gondola"
+    },
+    {
+        name:"Patnitop View Point",
+        img_url:"https://go2kashmir.com/kashmir/img/patnitop-view-bg.jpg",
+        loc_info:{
+            city:"Patnitop",
+            state:"Jammu & Kashmir"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Patnitop"
+    },
+    {
+        name:"Nubra Valley",
+        img_url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/b9/e6/1c/nubra-valley.jpg?w=1200&h=-1&s=1",
+        loc_info:{
+            city:"Leh",
+            state:"Ladakh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Nubra"
+    },
+    {
+        name:"Shanti Stupa",
+        img_url:"https://media1.thrillophilia.com/filestore/incr8e1ls7vwnw6hmzee2d76safs_shutterstock_28393609.jpg",
+        loc_info:{
+            city:"Leh",
+            state:"Ladakh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Shanti_Stupa,_Ladakh"
+    },
+     {
+        name:"Zuluk Zig Zag",
+        img_url:"https://live.staticflickr.com/2893/33383576360_a33a7eb84c_b.jpg",
+        loc_info:{
+            city:"Zuluk",
+            state:"Sikkim"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Dzuluk"
+    },
+    {
+        name:"Phugtal Monastery View",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMV-3ljaoj8Nc6_v9thO4-rQffHNfm6X3uMA&s",
+        loc_info:{
+            city:"Zanskar",
+            state:"Ladakh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Phugtal_Monastery"
+    },
+    {
+        name:"Chopta Tungnath",
+        img_url:"https://www.prasthanamindia.com/wp-content/uploads/2025/01/chopta-tungnath-chandra-shila-trekking-tour-package.jpg",
+        loc_info:{
+            city:"Chopta",
+            state:"Uttarakhand"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Chopta"
+    },
+    {
+        name:"Ranikhet Majkhali View",
+        img_url:"https://www.trodly.com/pictures/destination/x3200.jpg.pagespeed.ic.fyaXgIMKw7.jpg",
+        loc_info:{
+            city:"Ranikhet",
+            state:"Uttarakhand"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Ranikhet"
+    },
+    {
+        name:"Kurseong Eagle’s Crag",
+        img_url:"https://1001things.org/wp-content/uploads/2014/12/Watch-tower-at-Eagles-Crag-Kurseong.jpg",
+        loc_info:{
+            city:"Kurseong",
+            state:"West Bengal"
+        },
+        moreInfoUrl:"https://darjeelingoffbeat.com/eagles-crag-kurseong/"
+    },
+    {
+        name:"Kalimpong Deolo Hill",
+        img_url:"https://1001things.org/wp-content/uploads/2014/12/Deolo-featured-image-Kalimpong.jpg",
+        loc_info:{
+            city:"Kalimpong",
+            state:"West Bengal"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Deolo_Hill"
+    },
+    {
+        name:"Tawang Hilltop",
+        img_url:"https://www.tourism-of-india.com/pictures/travel_guide/attractions/thmb/tawang-84.jpeg",
+        loc_info:{
+            city:"Tawang",
+            state:"Arunachal Pradesh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Tawang"
+    },
+    {
+        name:"Bomdila View Point",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2RJZIP2ynF9-wPdSOIzamNqC17HkmrrHlkQ&s",
+        loc_info:{
+            city:"Bomdila",
+            state:"Arunachal Pradesh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Bomdila"
+    },
+    {
+        name:"Ziro Valley View",
+        img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A_cross_section_of_luch_green_valley_of_Ziro.jpg/800px-A_cross_section_of_luch_green_valley_of_Ziro.jpg",
+        loc_info:{
+            city:"Ziro",
+            state:"Arunachal Pradesh"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Ziro"
+    },
+    {
+        name:"Mount Kanchenjunga",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMeM8mHKsGGQDtKOhI8XKIl6umwDXzl8EcTA&s",
+        loc_info:{
+            city:"Yuksom",
+            state:"Sikkim"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Kangchenjunga"
+    },
+    {
+        name:"Sandakphu Peak",
+        img_url:"https://storage.googleapis.com/stateless-www-justwravel-com/2018/04/Day-4-View-from-Sandakphu-1024x683.jpg",
+        loc_info:{
+            city:"Manebhanjan",
+            state:"West Bengal"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Sandakphu"
+    },
+    {
+        name:"Lachen Gurudongmar",
+        img_url:"https://res.cloudinary.com/kmadmin/image/upload/v1618490887/kiomoi/Lachen_Gurudongmar_Lake_1618490886395.jpg",
+        loc_info:{
+            city:"Lachen",
+            state:"Sikkim"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Gurudongmar_Lake"
+    },
+    {
+        name:"Lachung Yumthang Valley",
+        img_url:"https://res.cloudinary.com/kmadmin/image/upload/v1618570894/kiomoi/Lachung_Yumthang_Valley_1618570893146.jpg",
+        loc_info:{
+            city:"Lachung",
+            state:"Sikkim"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Yumthang_Valley_of_Flowers"
+    },
+    {
+        name:"Rajmachi Fort View",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCbStY1ODQqT1fAUSRjDqn6A2sp_iRtUAZz6THerXs-HZPhW3Xk3-k82KMNEPaQA5CHmw&usqp=CAU",
+        loc_info:{
+            city:"Lonavala",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Rajmachi"
+    },
+    {
+        name:"Lion’s Point",
+        img_url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b2/55/22/photo0jpg.jpg?w=1200&h=-1&s=1",
+        loc_info:{
+            city:"Lonavala",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Lonavala"
+    },
+    {
+        name:"Panhalgad View",
+        img_url:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/48/e5/23/panhala-fort.jpg?w=1200&h=-1&s=1",
+        loc_info:{
+            city:"Kolhapur",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Panhala"
+    },
+    {
+        name:"Kalsubai Peak",
+        img_url:"https://gokyo.in/cdn/shop/articles/Untitled_design_7.jpg?v=1757934919&width=2048",
+        loc_info:{
+            city:"Bhandardara",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Kalsubai"
+    },
+    {
+        name:"Harihar Fort View",
+        img_url:"https://static.toiimg.com/thumb/msid-122145020,width-1280,height-720,resizemode-4/122145020.jpg",
+        loc_info:{
+            city:"Igatpuri",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Harihar_fort"
+    },
+    {
+        name:"Ratangad Fort View",
+        img_url:"https://hblimg.mmtcdn.com/content/hubble/img/Bhandardara/mmt/activities/m_activities_bhandardara_ratangad_fort_l_399_600.jpg",
+        loc_info:{
+            city:"Bhandardara",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Harihar_fort"
+    },
+    {
+        name:"Louisa Point",
+        img_url:"https://res.cloudinary.com/kmadmin/image/upload/v1723889069/kiomoi/Louisa_point_6461.jpg",
+        loc_info:{
+            city:"Matheran",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://ceb.wikipedia.org/wiki/Louisa_Point"
+    },
+    {
+        name:"Echo Point",
+        img_url:"https://static.toiimg.com/photo/msid-49179095,width-96,height-65.cms",
+        loc_info:{
+            city:"Matheran",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Matheran"
+    },
+    {
+        name:"Sajjangad Hill View",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqF3DteNYWcEDcx5zVKUA6CLqkvEHkvV85F3qjAyth7g3mdVBPT81fI2lHpGsQpnm4lH8&usqp=CAU",
+        loc_info:{
+            city:"Satara",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Sajjangad"
+    },
+    {
+        name:"Ajinkyatara Fort View",
+        img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIAfxAA5qzabdlcwnLtaiRsX31wRzGpK6qNQ&s",
+        loc_info:{
+            city:"Satara",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Ajinkyatara"
+    },
+    {
+        name:"Torana Fort View",
+        img_url:"https://www.treksandtrails.org/system/images/000/411/131/bc808389377ea7a94f3a9de318c2b38e/original/Zunzar_Machi_at_Torna.jpg",
+        loc_info:{
+            city:"Pune",
+            state:"Maharashtra"
+        },
+        moreInfoUrl:"https://en.wikipedia.org/wiki/Torna_Fort"
+    },
+];
---
+++ script.js
@@ -0,0 +1,80 @@
+import { viewPointsInfo } from "./data.js";
+
+const mainContainer = document.querySelector(".main-container");
+ 
+const createElement = (element) => document.createElement(element);
+
+
+// Create viewPoint Card
+
+const createViewpointCard = (viewPointsInfo) => {
+    viewPointsInfo.forEach(viewPoint => {
+
+        // create card container
+        const cardContainer = createElement("div");
+        cardContainer.classList.add("card-container");
+
+        // create image container
+        const imgContainer = createElement("div");
+        imgContainer.classList.add("img-div");
+
+        // create card image
+        const imgEleme = createElement("img");
+        imgEleme.setAttribute("src", viewPoint.img_url);
+        imgEleme.setAttribute("alt", viewPoint.name)
+        imgContainer.appendChild(imgEleme);
+ 
+
+        //create card details container
+        const cardDetails = createElement("div");
+        cardDetails.classList.add("details");
+
+        // card viewpoint name
+        const vpNameEle = createElement("h3")
+        vpNameEle.innerText = viewPoint.name;
+
+        // create loc-info container
+        const locInfoEle = createElement("div");
+        locInfoEle.classList.add("loc-info");
+
+        // card location info
+         const locEle = createElement("p")
+         if(viewPoint.loc_info){
+            locEle.innerText = `${viewPoint.loc_info.city}, ${viewPoint.loc_info.state}`;
+         } else{
+            locEle.innerText = "Location Information not Available"
+         }
+        // create location logo
+        const logoEle = createElement("i")
+        logoEle.classList.add("fa-solid", "fa-location-dot");
+        logoEle.setAttribute("id", "loc-logo");
+
+        locInfoEle.appendChild(locEle);
+        locInfoEle.appendChild(logoEle);
+        cardDetails.appendChild(vpNameEle);
+        cardDetails.appendChild(locInfoEle);
+
+        // create moreinfo container
+        const moreInfoEle = createElement("div");
+        moreInfoEle.classList.add("more-info");
+
+        moreInfoEle.addEventListener("click" ,() => {
+            window.location.href = `${viewPoint.moreInfoUrl}`
+        })
+        // create moreinfo button
+        const butEle = createElement("button");
+        butEle.innerText = "More Info";
+        const arrowEle = createElement("i");
+        arrowEle.classList.add("fa-solid", "fa-arrow-right");
+        butEle.appendChild(arrowEle);
+        moreInfoEle.appendChild(butEle);
+
+        cardContainer.appendChild(imgContainer);
+        cardContainer.appendChild(cardDetails);
+        cardContainer.appendChild(moreInfoEle);
+
+        mainContainer.appendChild(cardContainer);
+    });
+}
+
+createViewpointCard(viewPointsInfo);
